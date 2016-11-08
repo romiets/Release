4a8bb6d775952c2ca631a28c72f85656140405a5
Romeo Payawal <rpayawal@gmail.com>
2016-11-08 15:37:19 +0800

    Commit Master

---
+++ ClickApp/ClickApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClickApp.Models;
+namespace ClickApp.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private ClickAppDBEntities _entities = new ClickAppDBEntities();
+
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var _count = (from c in _entities.ClickApps
+                          select c).First();
+
+            return View(_count.Counter);
+        }
+
+        [HttpPost]
+        public ActionResult Update()
+        {
+            var _count = (from c in _entities.ClickApps
+                          select c).First();
+
+            if (_count.Counter < 10)
+            {
+                _count.Counter = _count.Counter + 1;
+                _entities.SaveChanges();
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
